1334e18164c7a75887cf7d995cb8b1e85e933c65
Matthieu <m.auvy@icloud.com>
2022-06-13 14:45:41 +0900

    created file structure and MVC templates

---
+++ model/Manager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wcoding\batch16\finalproject\Model;
+
+class Manager {
+    protected $_connection;
+
+
+    const DNAME = "batch16project";
+    const HOST = "localhost";
+    const LOGIN = "root";
+    const PWD = "";
+
+    protected function __construct() {
+        $this-> _connection = new \PDO('mysql:host=' .self::HOST. ';dbname='.self::DNAME.';charset=utf8', self::LOGIN, self::PWD);
+    }
+}
