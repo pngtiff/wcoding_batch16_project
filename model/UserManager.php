76394663750e71206b80719f8e9061c389e81021
bha80 <benjamin.ha1@gmail.com>
2022-06-15 16:05:51 +0900

    adding sign up files

--- model/UserManager.php
+++ model/UserManager.php
@@ -7,23 +7,34 @@ class UserManager extends Manager {
     {
         parent::__construct();
     }
+    protected function createUID() {
+        $uid = bin2hex(random_bytes(4));
+        $isUnique = $this->_connection->query("SELECT * FROM users WHERE uid='$uid'")->fetch(\PDO::FETCH_ASSOC) ? false : true;
+        while (!$isUnique) {
+            $uid = bin2hex(random_bytes(4));
+            $isUnique = $this->_connection->query("SELECT * FROM users WHERE uid='$uid'")->fetch(\PDO::FETCH_ASSOC) ? false : true;
+        }
+        return $uid;
+    }
     public function signUp($firstName, $lastName, $email, $password){
         $firstName = addslashes(htmlspecialchars(htmlentities(trim($firstName))));
         $lastName = addslashes(htmlspecialchars(htmlentities(trim($lastName))));
         $email = addslashes(htmlspecialchars(htmlentities(trim($email))));
         $password =  password_hash(htmlspecialchars($password), PASSWORD_DEFAULT);
+        $uid = $this->createUID(); 
 
             $response = $this->_connection->query("SELECT email, first_name, last_name FROM users WHERE email='$email'");
             if ($response->fetch(\PDO::FETCH_ASSOC)) {
                 header('Location:index.php');
             } else {
-                $response=$this->_connection->prepare("INSERT INTO users (password, email, firstName, lastName) VALUES (:password, :email, :first_name, :last_name)");
+                $response=$this->_connection->prepare("INSERT INTO users (password, email, first_name, last_name, uid) VALUES (:password, :email, :firstName, :lastName, :uid)");
                 $response->bindParam("firstName",$firstName, \PDO::PARAM_STR);
                 $response->bindParam("lastName",$lastName, \PDO::PARAM_STR);
                 $response->bindParam("email",$email, \PDO::PARAM_STR);
                 $response->bindParam("password",$password, \PDO::PARAM_STR);
+                $response->bindParam("uid",$uid, \PDO::PARAM_STR); 
                 $response->execute();
-                header('Location:signInView.php');
+                header('Location:index.php');
             }
         
     }
--- view/signUpView.php
+++ view/signUpView.php
@@ -5,19 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Account</title>
+    <link rel="stylesheet" href="public/style/style.css">
 </head>
 <body>
     <div class="create">
         <h1>Create Account</h1>
-        <form action="model/UserManager.php" method="post">
-            <input type="text" name="firstName" id="firstName" placeholder="First Name"> <br><br>
-            <input type="text" name="lastName" id="lastName" placeholder="Last Name"> <br><br>
-            <input type="text" name="email" id="email" placeholder="Email"> <br><br>
-            <input type="password" name="password"  id="password" placeholder="Password"> <br><br>
-            <input type="password" name="passwordConfirm"  id="passwordConfirm" placeholder="Password confirm"> <br><br>
-            <button type="submit" name="signUp" class="button">Create Account</button> <br><br>
+        <form id="signUpForm" action="index.php" method="post">
+            <div class="firstName">
+                <input type="text" name="firstName" id="firstName" placeholder="First Name" required>
+                <div class="nameError"><em>Please enter 2 or more characters (only letters)</em></div><br>
+            </div>
+            <div class="lastName">
+                <input type="text" name="lastName" id="lastName" placeholder="Last Name" required> 
+                <div class="lastNameError"><em>Please enter 2 or more characters (only letters)</em></div><br>
+            </div>
+            <div class="email">
+                <input type="text" name="email" id="email" placeholder="Email" required> 
+                <div class="emailError"><em>Please enter a valid email address</em></div><br>
+            </div>
+            <div class="password">
+                <input type="password" name="password"  id="password" placeholder="Password" required> 
+                <div class="passwordError"><em>Must be a minimum of 8 characters: at least 1 uppercase, at least 1 lowercase, at least 1 digit, at least 1 special character: !@#$%^&*-</em></div><br>
+            </div>
+            <div class="passwordConfirm">
+                <input type="password" name="passwordConfirm"  id="passwordConfirm" placeholder="Password Confirm" required> 
+                <div class="passwordConfirmError"><em>Password must match</em></div><br>
+            </div>
+            <button id="submit "type="submit" name="signUp" class="button">Create Account</button> <br><br>
+            <input type="hidden" name="action" value="signUp">
         </form>
-        <button type="submit" name="googleSignup" class="button" onclick="view/googleLoginButton">Log in using Google</button>
+        <button id="submit" type="submit" name="googleSignup" class="button" onclick="view/googleLoginButton">Sign in using Google</button> <br><br>
+        <button id="reset" type="reset" onclick="resetForm()">Reset the form</button>
     </div>
+
+    <script src="public/js/signUp.js"></script>
 </body>
 </html>
