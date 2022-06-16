b47f67c7ae1160daba2dabdd45a9f7fe9708a71e
Matthieu <m.auvy@icloud.com>
2022-06-16 15:14:37 +0900

    improved landing page design

--- view/indexView.php
+++ view/indexView.php
@@ -5,17 +5,24 @@
 <?php ob_start();?>
 
 <section class="flexColumn">
-    <div class="searchBarContainer">
-        <input type="text" name="searchbar" id="searchBar" placeholder="SEARCH">
-    </div>
 
-    <div class="slideshow">
-      <div id="slide1" class="slide"></div>
-    </div>
+  <div class="landingHeader">
+    <h1>WELCOME TO OUR SUPER WEBSITE. START YOUR ROOM SEARCH TODAY !</h1>
+    <br>
+    <h3>Lorem ipsum doincididim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+  </div>
+  
+  <div class="searchBarContainer">
+      <input type="text" name="searchbar" id="searchBar" placeholder="Start your search here">
+  </div>
+
+  <div class="slideshow">
+    <div id="slide1" class="slide"></div>
+  </div>
 
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 
   </section>
