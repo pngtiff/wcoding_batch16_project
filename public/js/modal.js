955c5aed8cdfb3beebbcb0933561cd93474ed0de
Matthieu <m.auvy@icloud.com>
2022-06-16 10:27:16 +0900

    CSS fixes on classes names and cleaning up the CSS file

--- public/js/modal.js
+++ public/js/modal.js
@@ -5,17 +5,20 @@ function opacity (opacity) {
 //// open the modal on SignUp/Sign In button click
 document.getElementById("signUpButton").addEventListener("click", function() {
     document.getElementById("modalBox").style.display = "block";
+    document.querySelector("body").style.overflowY = "hidden";
     setTimeout(opacity, 200, 1)
 })
 
 document.getElementById("signInButton").addEventListener("click", function() {
     document.getElementById("modalBox").style.display = "block";
+    document.querySelector("body").style.overflowY = "hidden";
     setTimeout(opacity, 200, 1)
 })
 
 // close the modal when click X
 document.querySelector(".close").addEventListener("click", function() {
     document.getElementById("modalBox").style.display = "none";
+    document.querySelector("body").style.overflowY = "visible";
     opacity(0);
 })
 
@@ -23,6 +26,7 @@ document.querySelector(".close").addEventListener("click", function() {
 window.addEventListener("click", function(e) {
   if (e.target == document.getElementById("modalBox")) {
     document.getElementById("modalBox").style.display = "none";
+    document.querySelector("body").style.overflowY = "visible";
     opacity(0);
   }
 })
--- view/header.php
+++ view/header.php
@@ -1,11 +1,11 @@
-<header> 
+<header class="flexColumn"> 
     <h1>Welcome to ROOM EZ !</h1>
     <h2>header</h2> 
     
     <div class="menu">
         <button id="signUpButton"><a href="#">REGISTER</a></button>
-        <?php echo (isset($_REQUEST['action']) && $_REQUEST['action'] == "login") ? '<button id="signOutButton"><a href="#">Sign Out</a></button>' : '<button id="signInButton"><a href="#">Sign In</a></button>'; ?>
-        <?php echo (isset($_REQUEST['action']) && $_REQUEST['action'] == "login") ? '<button id="settingsButton"><a href="#">Settings</a></button>' : ''; ?>
+        <?php echo (isset($_REQUEST['action']) && $_REQUEST['action'] == "login") ? '<button id="signOutButton">Sign Out/button>' : '<button id="signInButton">Sign In</button>'; ?>
+        <?php echo (isset($_REQUEST['action']) && $_REQUEST['action'] == "login") ? '<button id="settingsButton">Settings</button>' : ''; ?>
     </div>
 </header>
 
