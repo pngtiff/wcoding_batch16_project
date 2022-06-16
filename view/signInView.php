48343d5f883573da963e4920f9a26d8844503da0
YJ <whyjay523@gmail.com>
2022-06-16 10:23:54 +0900

    sign in updated

--- view/signInView.php
+++ view/signInView.php
@@ -5,7 +5,6 @@
             <input type="password" name="password"  id="password" placeholder="Password"> <br><br>
             <!-- <a href="#">Forgot your password?</a> <br><br> -->
             <button class="button" type="submit">Sign In</button>
-            <button type="submit" class="googleButton">Sign In with Google</button>
             <input type="hidden" name="action" value="signIn">
             <span class="errorText">Please try again</span>    
         </form>
