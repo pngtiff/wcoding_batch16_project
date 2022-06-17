4000ca22a544d076043dd8939c57ac5807b42b8b
bha80 <benjamin.ha1@gmail.com>
2022-06-17 11:43:40 +0900

    updating header

--- view/header.php
+++ view/header.php
@@ -1,3 +1,4 @@
+
 <header class="flexColumn"> 
     <!-- <h2>BATCH 16 (awesome) PROJECT : ROOM EZ !</h2> -->
     <div class="headerContainer"> 
@@ -12,13 +13,13 @@
                 <div class="dropdown-content">
                     <a href="#intro">Who are we?</a>
                     <a href="">What do we do?</a>
-                    <a href="">Contact us</a>
+                    <a href=".contact">Contact us</a>
                 </div>
             </div>
             <div class="dropdown2">
                 <button class="dropbtn2">Browse</button>
                 <div class="dropdown-content2">
-                    <a href="">Search Most Recent Listings</a>
+                    <a href="#propertiesContainer">Search Most Recent Listings</a>
                     <a href="">Search All Listings</a>
                     <a href="">Search for a renter</a>
                     <a href="">Find a Roommate</a>
@@ -34,7 +35,7 @@
             <div class="dropdown4">
                 <button class="dropbtn4">Account</button>
                 <div class="dropdown-content4">
-                    <a href="">View My Profile</a>
+                    <a href="view/profileFormView.php">View My Profile</a>
                     <a href="">Edit My Profile</a>
                     <a href="">Logout</a>
                 </div>
