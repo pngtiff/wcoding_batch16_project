b053b91da99b20a75177ba58a1d46182888b349d
JeongGeon Oh <jeoh0605@gmail.com>
2022-06-21 16:24:50 +0900

    updated multiselect for user languages

--- public/js/createProfile.js
+++ public/js/createProfile.js
@@ -18,7 +18,7 @@ function checkDays() {
     } else if (days31.includes(birthMonth)) {
         daysInMonth = 31;
     }
-   
+
     for (let i = 1; i <= daysInMonth; i++) {
         let option = document.createElement("option");
         option.value = i;
@@ -39,13 +39,16 @@ let months = document.querySelector('#month');
 let days = document.querySelector('#day');
 let gender = document.querySelector('input[name="gender"]:checked');
 let languages = document.querySelector('#language');
+let options = document.querySelectorAll('#language option');
+let langArray = [];
+let userLang = document.querySelector('#userLang');
 let bio = document.querySelector('#bio');
 let imgContainer = document.querySelector('#profilePhoto div');
 
 
 // change the photo by choosing the different files
-file.addEventListener('change', function(){   
-    imgContainer.innerHTML="";
+file.addEventListener('change', function () {
+    imgContainer.innerHTML = "";
     const chosenFile = this.files[0];
     let fileSize = chosenFile.size;
     let fileType = chosenFile.name.split('.').pop();
@@ -58,10 +61,10 @@ file.addEventListener('change', function(){
 
     let img = document.createElement('img');
 
-    if(chosenFile){
+    if (chosenFile) {
         const reader = new FileReader();
 
-        reader.addEventListener('load', function(){
+        reader.addEventListener('load', function () {
             img.setAttribute('src', reader.result);
         });
 
@@ -95,12 +98,24 @@ years.addEventListener('change', () => {
     checkDays();
 })
 
+languages.addEventListener('change', (e) => {
+    for (i = 0; i < options.length; i++) {
+        if(options[i].id === e.target.value) {
+            options[i].setAttribute('disabled', 'disabled');
+            langArray.push(e.target.value);
+        }
+    }
+    userLang.value = langArray;
+})  
+
 profileForm.addEventListener('submit', (e) => {
-    if (!(validNum || gender.value || languages.value || bio.value)) {
+    if (!(validNum || gender.value || userLang.value || bio.value)) {
         e.preventDefault();
         let alert = document.querySelector('#newProfile p');
         if (!alert.textContent) {
             alert.textContent = "Please fill out all fields";
         }
-    } 
+    }
 })
+
+
