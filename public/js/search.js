919d7edfbc5a52136c7ec6fb619a704489f1b9d5
Matthieu <m.auvy@icloud.com>
2022-06-22 12:00:00 +0900

    added AJAX Refresh for search results

--- public/js/search.js
+++ public/js/search.js
@@ -1,31 +1,42 @@
 /////////// trigger search on icon click//////////////
 
-document.querySelector(".searchButton").addEventListener("click", function(e) {
+function loadSearch() {
     let search = (document.querySelector("#searchBar").value != "") ? document.querySelector("#searchBar").value : "anywhere"////// content of the search bar
     let rangeMin = (document.querySelector("#rentRange").value != "any") ? parseInt(document.querySelector("#rentRange").value)-500000 : "any"
     let rangeMax = (document.querySelector("#rentRange").value != "any") ? parseInt(document.querySelector("#rentRange").value) : "any"
     let propertyType = (document.querySelector("#propertyType").value != "any") ? document.querySelector("#propertyType").value : "any"
     let roomType = (document.querySelector("#roomType").value != "any") ? document.querySelector("#roomType").value : "any"
 
-
-    window.location = `index.php?action=search&search=${search}&rangeMin=${rangeMin}&rangeMax=${rangeMax}&propertyType=${propertyType}&roomType=${roomType}` 
-     ////// redirect to search results
-})
-
-
-/// ENTER KEY ////////
-document.getElementById("searchBar").addEventListener("keyup", function(e) {
+    // e.preventDefault();
+    // window.location = `index.php?action=search&search=${search}&rangeMin=${rangeMin}&rangeMax=${rangeMax}&propertyType=${propertyType}&roomType=${roomType}` 
+    // window.history.pushState('', '', `index.php?action=search&search=${search}&rangeMin=${rangeMin}&rangeMax=${rangeMax}&propertyType=${propertyType}&roomType=${roomType}` );
     
+    const xhr = new XMLHttpRequest()
+
+    ////// LOAD THE ROUTER with action "Search"////////
+    xhr.open("get", `index.php?action=search&search=${search}&rangeMin=${rangeMin}&rangeMax=${rangeMax}&propertyType=${propertyType}&roomType=${roomType}`)
+
+    ////// Response = searchResultsCard with $_get parameters loaded in xhr.open above //////////
+    xhr.addEventListener("load", function(e) {
+        if (e.target.status === 200) {
+            document.querySelector("section").innerHTML = ""
+            document.querySelector("section").innerHTML = xhr.response
+        } else {
+            console.log("bad Request")
+        }
+    })
+
+    xhr.send(null)
+}
+
+//// CLICK ICON///////
+document.querySelector(".searchButton").addEventListener("click", loadSearch)
+ 
+/// ENTER KEY ////////
+document.getElementById("searchBar").addEventListener("keyup", function(e) {   
     if (e.key == "Enter")
     {
-        let search = (document.querySelector("#searchBar").value != "") ? document.querySelector("#searchBar").value : "anywhere" ////// content of the search bar
-        let rangeMin = (document.querySelector("#rentRange").value != "any") ? parseInt(document.querySelector("#rentRange").value)-500000 : "any"
-        let rangeMax = (document.querySelector("#rentRange").value != "any") ? parseInt(document.querySelector("#rentRange").value) : "any"
-        let propertyType = (document.querySelector("#propertyType").value != "any") ? document.querySelector("#propertyType").value : "any"
-        let roomType = (document.querySelector("#roomType").value != "any") ? document.querySelector("#roomType").value : "any"
-    
-    
-        window.location = `index.php?action=search&search=${search}&rangeMin=${rangeMin}&rangeMax=${rangeMax}&propertyType=${propertyType}&roomType=${roomType}` 
+        loadSearch()  
     }
 })
 
--- view/searchView.php
+++ view/searchView.php
@@ -3,27 +3,7 @@
 <?php ob_start(); ?>
 
 <section id='searchResults'>
-<?php
-
-    if(count($properties)>0){
-        ?>
-        
-        <h2>Search Results for <?= $_REQUEST['search'] ?></h2>
-        <div id='propertiesSearchList'>
-            <?php
-            foreach($properties as $property)
-            {
-                include('propertyCard.php');
-            } ?>
-        </div>
-        
-    <?php
-    }else {
-        ?>
-        <p>No Search Results Yet for <b><?= $_REQUEST['search'] ?></b> with the Selected Filters ! Try to expand your Search.</p>
-        <?php
-    } ?>
-
+    <?php require('searchResultsCard.php');?>
 </section>
 
 <?php $content = ob_get_clean();?>
