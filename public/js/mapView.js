5d6a904b68e5a875342ba7825604b39644545a2c
Matthieu <m.auvy@icloud.com>
2022-06-29 11:24:53 +0900

    added mapView for search Results

--- public/js/mapView.js
+++ public/js/mapView.js
@@ -1,28 +1,5 @@
 ////////// AJAX 1 : GET ZIPCODE FROM DATABASE /////////////
 
-async function createMap() {
-
-    const params = new URLSearchParams(window.location.search);
-    const propId = parseInt(params.get("propId"));
-    const url = "index.php?action=getZipCode&propId=" + propId
-
-    let response = await fetch(url);
-    let zipcode = await response.text();
-    zipcodeGeotagging(zipcode)
-}
-
-async function zipcodeGeotagging(zipcode) {
-    const url = "https://geocode.xyz/" + zipcode + "?region=KR&json=1"
-   
-    let response = await fetch(url);
-    let coordinates = await response.json();
-    console.log(coordinates)
-    let lat = coordinates['latt']
-    let long = coordinates['longt']
-    
-    kakaoMap(lat,long)
-}
-
 function kakaoMap(lat,long) {
     var mapContainer = document.getElementById('map'),
         mapOption = { 
@@ -45,5 +22,15 @@ function kakaoMap(lat,long) {
 }
 
 
-createMap()
 
+// async function zipcodeGeotagging(zipcode) {
+//     const url = "https://geocode.xyz/" + zipcode + "?region=KR&json=1"
+   
+//     let response = await fetch(url);
+//     let coordinates = await response.json();
+//     console.log(coordinates)
+//     let lat = coordinates['latt']
+//     let long = coordinates['longt']
+    
+//     kakaoMap(lat,long)
+// }
